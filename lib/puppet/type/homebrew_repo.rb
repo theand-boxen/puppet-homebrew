9b37f0f93c4eb75e9254f9af84f78a9c2f1e2610
Heesang Chae <theand@gmail.com>
2015-12-14 18:38:16 +0900

    debug

--- lib/puppet/type/homebrew_repo.rb
+++ lib/puppet/type/homebrew_repo.rb
@@ -24,7 +24,7 @@ Puppet::Type.newtype(:homebrew_repo) do
 
     defaultto do
       user = Facter.value(:boxen_user) || Facter.value(:id) || "root"
-      puts "Repo User: " + user
+      puts "Repo User: " + user.to_s
       user
     end
   end
